7bc183d42f7efa50b3bbc7179bc91f2ea3a2525c
unknown <abdelrahmanahmedhamdy99@gmail.com>
2021-12-13 18:14:42 +0200

    layered panel .. no thing in the project

---
+++ layeredPanel/src/Window.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Window extends JFrame {
+    Window(String title) {
+        this.setTitle(title);
+        // this.setVisible(true);
+        this.setSize(800, 500);
+        this.getContentPane().setBackground(new Color(194, 8, 199));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    Window(String title, int r, int g, int b) {
+        this.setTitle(title);
+        this.setVisible(true);
+        this.setSize(800, 500);
+        this.getContentPane().setBackground(new Color(r, g, b));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    public static void changeBackgroundRandomly(Window window) {
+        window.setBackground(new Color((int)(Math.random()*250)+1, (int)(Math.random()*250)+1, (int)(Math.random()*250)+1));
+    }
+
+}
